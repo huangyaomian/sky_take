b38253e59d888451eb2a45a7790af30032f85aa1
yaomian <345249887@qq.com>
2024-01-13 10:25:31 +0800

    导入客户端的查询订单，再来一单等代码 导入支付相关代码（但是由于微信的限制是无法支付的）

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -21,4 +21,7 @@ public interface UserMapper {
     @Select("select * from user where openid = #{openid}")
     User getByOpenid(String openid);
 
+    @Select("select * from user where id = #{id}")
+    User getById(Long id);
+
 }
